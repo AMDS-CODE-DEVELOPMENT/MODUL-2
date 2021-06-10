689273454a2ead6abdc7c5e47f89222a20bd3001
Ahmad_Setiaji-(AMDS) <54342235+AMDS-CODE-DEVELOPMENT@users.noreply.github.com>
2021-06-10 19:03:55 +0800

    Penanganan ekspresi. try catch

--- src/model/MahasiswaBeraksi.java
+++ src/model/MahasiswaBeraksi.java
@@ -5,21 +5,22 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MahasiswaBeraksi {
-    public static void main(String[] args) throws ParseException {
-        String polaTanggal = "dd-MM-yyyy";
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(polaTanggal);
-
+    public static void main(String[] args) {
         Mahasiswa mahasiswa = new Mahasiswa();
         mahasiswa.npm = "19630170";
         mahasiswa.nama = "Setiaji";
-        mahasiswa.tanggallahir = simpleDateFormat.parse("23-09-2000");
 
-        System.out.println("Nama\t\t\t: "+mahasiswa.nama);
-        System.out.println("Nama\t\t\t: "+mahasiswa.getNama());
-        System.out.println("NPM\t\t\t\t: "+mahasiswa.npm);
-        System.out.println("Tanggal Lahir\t: "+simpleDateFormat.format(mahasiswa.tanggallahir));
+        String polaTanggal = "dd-MM-yyyy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(polaTanggal);
+        try {
+            mahasiswa.tanggallahir = simpleDateFormat.parse("09-23-2000");
+            System.out.println("Nama\t\t\t: "+mahasiswa.nama);
+            System.out.println("NPM\t\t\t\t: "+mahasiswa.npm);
+            System.out.println("Tanggal Lahir\t: "+simpleDateFormat.format(mahasiswa.tanggallahir));
 
-
-        mahasiswa.menyapa();
+            mahasiswa.menyapa();
+        } catch (ParseException e) {
+            System.err.println("Format Tanggal Salah");
+        }
     }
 }
